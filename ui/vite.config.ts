817a747ce52499d82621105c2b76f7c13ad32147
tatevosyan_a <tatevosyan_a@stilsoft.ru>
2026-03-16 23:54:33 +0300

    feat(UI): добавлен SideBar template & dropdown-menu + theme mode-toggle

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "node:path";
+import tanstackRouter from "@tanstack/router-plugin/vite";
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+      // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+      tanstackRouter({
+          target: 'react',
+          autoCodeSplitting: true,
+      }),
+      tailwindcss(),
+      react()
+  ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    }
 })
